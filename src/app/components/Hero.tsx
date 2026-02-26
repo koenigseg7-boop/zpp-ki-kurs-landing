83cd8f8a10410736c9b3a1ce79c7fe4a589b3ee2
Dein Name <Koenigseg7@gmail.com>
2026-02-26 19:30:41 +0100

    Datum auf 04.04 geändert, Datumsauswahl entfernt

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -5,7 +5,7 @@ import { HologramCard } from '@/app/components/HologramCard';
 import { AnimatedCounter } from '@/app/components/AnimatedCounter';
 import { ScrollIndicator } from '@/app/components/ScrollIndicator';
 
-const heroImage = '/images/hero.png';
+import heroImage from 'figma:asset/64daa4409054f7283e007704247e39484f16fd92.png';
 
 interface HeroProps {
   onBookClick: () => void;
